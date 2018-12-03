4ff37e03fa665a7b6f44d8c62e91e910ca379ff2
thibault <thibault.gerrier@student.uibk.ac.at>
2018-12-03 14:26:17 +0100

    saving to graphdb functionality

--- client/src/helpers/semantify.ts
+++ client/src/helpers/semantify.ts
@@ -136,3 +136,28 @@ export const saveAnnToSemantifyWebsite = async (
     console.log(e);
   }
 };
+
+const defaultGraphDB = {
+  repositoryId: 'annotation-editor',
+  url: 'http://localhost:7200', // http://graphdb.sti2.at:8080/
+};
+
+export const saveToGraphDb = async (annotations: any): Promise<boolean> => {
+  try {
+    await axios({
+      method: 'post',
+      url: `${defaultGraphDB.url}/repositories/${
+        defaultGraphDB.repositoryId
+      }/statements`,
+      headers: {
+        Accept: 'application/ld+json',
+        'Content-Type': 'application/ld+json',
+      },
+      data: annotations,
+    });
+    return true;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+};
